67a626ef2a55f9a5591e9583ee5f9a5a5124c8db
Jakub Konka <jakub.konka@golem.network>
2019-10-01 17:49:17 +0200

    Workshop (#8)
    * Format x- and y-axis, and attach x-label
    
    * Re-do distance and TTI calculations
    
    * Display first and last x label
    
    * Clean label formatting
    
    * Update y-label

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use chrono::NaiveTime;
 use gmorph::{Decrypt, Enc, Encrypt, KeyPair};
 use gudot_utils::{deserialize_from_file, serialize_to_file};
 use plotters::prelude::*;
@@ -102,9 +103,9 @@ fn generate_impl() -> Result<()> {
 
     //    let x = vec!(1,2,3,4);
     //    let y = vec!(2,4,6,8);
-    let v = 2.71;
-    let t0 = 9 * 3600;
-    let d0 = 6624;
+    let v = 5.4;
+    let t0 = 9 * 3600 + 30 * 60;
+    let d0 = 6500;
 
     let mut x = Vec::new();
     let mut y = Vec::new();
@@ -113,7 +114,7 @@ fn generate_impl() -> Result<()> {
     let normal =
         Normal::new(0.0, 2.0).map_err(|err| format!("Couldn't create noise source: {:?}", err))?;
 
-    for i in 1..100 {
+    for i in 0..600 {
         let t = t0 + i;
         let noise = normal.sample(&mut rng);
         let dd = (v * (i as f64) + noise).round() as u32;
@@ -264,11 +265,24 @@ fn plot_impl(with_x_range: Option<WithXRange>) -> Result<()> {
     root.fill(&WHITE).map_err(fmt_plotting_err)?;
     let root = root.margin(20, 20, 20, 20);
     let mut chart = ChartBuilder::on(&root)
-        .x_label_area_size(30)
-        .y_label_area_size(30)
-        .build_ranged(x_range.min..x_range.max, y_range.min..y_range.max)
+        .x_label_area_size(40)
+        .y_label_area_size(60)
+        .build_ranged(
+            x_range.min - 30.0..x_range.max + 30.0,
+            y_range.min..y_range.max,
+        )
         .map_err(fmt_plotting_err)?;
-    chart.configure_mesh().draw().map_err(fmt_plotting_err)?;
+    chart
+        .configure_mesh()
+        .x_label_formatter(&|secs| {
+            let nt = NaiveTime::from_num_seconds_from_midnight(secs.round() as u32, 0);
+            nt.format("%H:%M:%S").to_string()
+        })
+        .x_desc("Time, hh:mm:ss")
+        .y_label_formatter(&|distance| format!("{}", distance.round() as u64))
+        .y_desc("Distance from Osaka, hm")
+        .draw()
+        .map_err(fmt_plotting_err)?;
     chart
         .draw_series(PointSeries::of_element(x_y, 2, &BLUE, &|c, s, st| {
             return EmptyElement::at(c) + Circle::new((0, 0), s, st.filled());
